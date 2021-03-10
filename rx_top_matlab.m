ce0d85ee80a5c8a2671006a6022819e654593074
s160067 <36848216+S160067@users.noreply.github.com>
2021-03-10 13:27:01 +0100

    Init
    initial top level matlab scripts

---
+++ rx_top_matlab.m
@@ -0,0 +1,21 @@
+%{
+main Matlab document RX side, this will function as a top level document mostly for
+connection inputs and outputs of functions
+
+
+%}
+%% ADC/file processing  
+
+
+
+
+
+
+%% demodulator
+
+
+%% demapping
+
+
+%% output processing
+
---
+++ tx_top_matlab.m
@@ -0,0 +1,21 @@
+%{
+main Matlab document RX side, this will function as a top level document mostly for
+connection inputs and outputs of functions
+
+
+%}
+%% Input processing 
+
+
+
+
+
+
+%% mapper
+
+
+%% Modulation
+
+
+%% DAC/egress
+
